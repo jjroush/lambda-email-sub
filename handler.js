a08d7886be27df91937b7403c0ee7a7eada2fdf0
Jacob Roush <jacobroushmail@gmail.com>
2019-02-27 08:16:04 -0600

    progress

--- handler.js
+++ handler.js
@@ -1,4 +1,6 @@
 'use strict';
+const config = require('./config')
+const mailgun = require('mailgun-js')({apiKey: config.MAIL_GUN_KEY, domain: config.DOMAIN});
 
 module.exports.hello = async (event, context) => {
   return {
@@ -7,8 +9,21 @@ module.exports.hello = async (event, context) => {
       message: 'Go Serverless v1.0! Your function executed successfully!',
       input: event,
     }),
+    
   };
+
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
+
+const list = mailgun.lists(`mylist@${config.domain}`);
+
+const bob = {
+  subscribed: true,
+  address: 'bob@example.com'
+};
+
+list.members().create(bob, function (error, data) {
+  console.log(data);
+});
