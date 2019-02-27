7bc875c714d57199022d394aef1c5767b8d8562b
Jacob Roush <jacobroushmail@gmail.com>
2019-02-27 15:31:57 -0600

    mod

--- handler.js
+++ handler.js
@@ -11,10 +11,6 @@ module.exports.hello = async (event, context) => {
     }),
     
   };
-
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
 
 const list = mailgun.lists(`mylist@${config.domain}`);
