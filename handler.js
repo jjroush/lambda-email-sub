11cbaf1b8f9ee14d459660d49bc55a5ad8237c1f
Jacob Roush <jacobroushmail@gmail.com>
2019-03-08 21:30:13 -0600

    update

--- handler.js
+++ handler.js
@@ -2,24 +2,34 @@
 const config = require('./config')
 const mailgun = require('mailgun-js')({apiKey: config.MAIL_GUN_KEY, domain: config.DOMAIN});
 
-module.exports.emailSub = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-    
+
+
+module.exports.emailSub = (event, context, callback) => {
+  const list = mailgun.lists(`${config.NAME}@${config.DOMAIN}`);
+  console.log('address', event.body);
+
+  const newSub = {
+    subscribed: true,
+    address: event.body,
+    name: '',
+    vars: {}
   };
+
+
+
+  list.members().create(newSub).then(function (data) {
+
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin' : '*', // Required for CORS support to work
+    },
+    body: JSON.stringify({ "message": "Hello World!" })
+  };
+
+
+  callback(null, response);
+
+  });
 };
-
-// const list = mailgun.lists(`${config.NAME}@${config.DOMAIN}`);
-
-// const bob = {
-//   subscribed: true,
-//   address: 'bob@example.com'
-// };
-
-// list.members().create(bob, function (error, data) {
-//   console.log(data);
-// });
