bc4b574a9c485c43898b5e2eb8ceebe6870dd12e
Jacob Roush <jacobroushmail@gmail.com>
2019-04-01 18:57:06 -0500

    added public key for validation

--- handler.js
+++ handler.js
@@ -1,12 +1,11 @@
 'use strict';
 const config = require('./config')
-const mailgun = require('mailgun-js')({apiKey: config.MAIL_GUN_KEY, domain: config.DOMAIN});
+const mailgun = require('mailgun-js')({apiKey: config.MAIL_GUN_KEY, domain: config.DOMAIN, publicApiKey: config.MAIL_GUN_PUB_KEY});
 
 
 
 module.exports.emailSub = (event, context, callback) => {
   const list = mailgun.lists(`${config.NAME}@${config.DOMAIN}`);
-  console.log('address', event.body);
 
   const newSub = {
     subscribed: true,
@@ -16,20 +15,44 @@ module.exports.emailSub = (event, context, callback) => {
   };
 
 
+ mailgun.validate(event.body, function (err, body) {
+   console.log(body);
+    if (body && body.is_valid) {
+      list.members(event.body).info(function (err, members) {
+        if (members.member.subscribed) {
+          const response = {
+            statusCode: 200,
+            headers: {
+              'Content-Type': 'application/json',
+              'Access-Control-Allow-Origin' : '*', // Required for CORS support to work
+            },
+            body: JSON.stringify({ "message": "Already Added" })
+          };
+          callback(null, response);
+        }
+      });
+      list.members().create(newSub).then(function (err, data) {
+        const response = {
+          statusCode: 200,
+          headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin' : '*', // Required for CORS support to work
+          },
+          body: JSON.stringify({ "message": "Success" })
+        };
+        callback(null, response);
+        }).catch(console.log(err));
+    } else {
+      const response = {
+        statusCode: 422,
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin' : '*', // Required for CORS support to work
+        },
+        body: JSON.stringify({ "message": "Invalid Email" })
+      };
+      callback(null, response);
+    }
+});
 
-  list.members().create(newSub).then(function (data) {
-
-  const response = {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin' : '*', // Required for CORS support to work
-    },
-    body: JSON.stringify({ "message": "Hello World!" })
-  };
-
-
-  callback(null, response);
-
-  });
 };
